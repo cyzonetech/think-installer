c910a43b4f50233ba39fcbf8544b0918dbec3279
cyzonetech <30819954+cyzonetech@users.noreply.github.com>
2018-12-15 23:02:42 +0800

    修正think-testing包名
    修正think-testing包名

--- src/ThinkTesting.php
+++ src/ThinkTesting.php
@@ -23,7 +23,7 @@ class ThinkTesting extends LibraryInstaller
      */
     public function getInstallPath(PackageInterface $package)
     {
-        if ('topthink/think-testing' !== $package->getPrettyName()) {
+        if ('cyzonetech/think-testing' !== $package->getPrettyName()) {
             throw new \InvalidArgumentException('Unable to install this library!');
         }
 
